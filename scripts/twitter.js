6fa518843e0f85fbd816f8882a664081d393b693
Claire Opila <claireopila@dhcp-ccc-2760.eduroam.cornell.edu>
2016-03-05 17:59:50 -0500

    adding twitter oauth

--- scripts/twitter.js
+++ scripts/twitter.js
@@ -1,8 +1,21 @@
 var ref = new Firebase("https://social-informor.firebaseio.com");
-ref.authWithOAuthRedirect("twitter", function(error) {
-  if (error) {
-    console.log("Login Failed!", error);
-  } else {
-    // We'll never get here, as the page will redirect on success.
-  }
-});
+
+(function() {
+    $(&quot;#sign-in-with-twitter&quot;).on(&quot;click&quot;, function() {
+        ref.authWithOAuthRedirect("twitter", function(error) {
+	  		if (error) {
+	    		console.log("Login Failed!", error);
+	  		} else {
+	    // We'll never get here, as the page will redirect on success.
+	  		}
+		});
+ 	});
+ });
+   
+
+
+ // $(function() {
+ //                $(&quot;#sign-in-with-twitter&quot;).on(&quot;click&quot;, function() {
+ //                    window.location.href = &quot;<some domain>/request-token&quot;;
+ //                });
+ //            });
