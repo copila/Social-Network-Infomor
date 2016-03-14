d7f03507d1a3d94dfe95e777d19d5a8fcd2a2599
Claire Opila <claireopila@dhcp-ccc-2705.eduroam.cornell.edu>
2016-03-14 02:03:27 -0400

    adding files

--- scripts/Facebook.js
+++ scripts/Facebook.js
@@ -311,19 +311,25 @@ function displayHotness(array){
   }
 }
 
-$(document).ready(function() {
-  $('#hot_artists').on('click', queryFireBase());
 
-  function queryFireBase(){
-    console.log("queryFireBase called!");
-    window.alert("you queried firebase");
-    var artistHotnessRef = new Firebase("https://social-informor.firebaseio.com/"+name+"artistHotness");
-    artistHotnessRef.orderByChild("hotttnesss").limitToLast(10).on("child_added", function(snapshot) {
-      var object = snapshot.val();
-      console.log("snapshot value is: ", JSON.stringify(object));
-    });
-  }
+
+$(document).ready(function() {
+  $('#hot_artists').on('click', 'input', function() { queryFireBase()});
 });
+
+
+
+
+function queryFireBase(){
+  console.log("queryFireBase called!");
+  window.alert("you queried firebase");
+  var artistHotnessRef = new Firebase("https://social-informor.firebaseio.com/"+name+"artistHotness");
+  artistHotnessRef.orderByChild("hotttnesss").limitToLast(10).on("child_added", function(snapshot) {
+    var object = snapshot.val();
+    console.log("snapshot value is: ", JSON.stringify(object));
+  });
+}
+
 
 
 
