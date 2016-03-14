f59b5d4c47eb22aa8fc31fff2e9d8b3b3ec0e4fd
zaidhaque <zaidhaque@gmail.com>
2016-03-14 02:45:05 -0400

    modified async call

--- scripts/echonest.js
+++ scripts/echonest.js
@@ -58,13 +58,10 @@ $(document).ready(function() {
 		artist = "Radiohead";
 		artists = ["Radiohead", "Led+Zeppelin", "The+Beatles"];
 		for (var i = 0; i < artists.length; i++) {
-			(function () {
-				console.log("artist searched is: " + artists[i]);
-				url2 = url+artistSyntax+songs+APIpart+"&name="+artists[i]+"&results=1"+"&start="+start;
-				console.log("url is: " + url2);
-				getURLTracks(url2, artists[i]);	
-				window.alert("top tracks for: " + artists[i] +"!");
-			});
+			console.log("artist searched is: " + artists[i]);
+			url2 = url+artistSyntax+songs+APIpart+"&name="+artists[i]+"&results=1"+"&start="+start;
+			console.log("url is: " + url2);
+			getURLTracks(url2, artists[i]);	
 		}
 		document.getElementById('artiste').value = "";
 	}
@@ -74,6 +71,7 @@ $(document).ready(function() {
 		$.get(url2, function(data, status){
         console.log("Data: " + JSON.stringify(data) + "\nStatus: " + status);
         formatResultsTracks(data, artist);
+        window.alert("top tracks for: " + artist +"!");
     	});
 	}
 
