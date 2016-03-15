8fbbb7e59f71d0a2492b710873a5c779c11b3053
zaidhaque <zaidhaque@gmail.com>
2016-03-14 23:46:13 -0400

    formatting and images

--- scripts/Facebook.js
+++ scripts/Facebook.js
@@ -334,6 +334,7 @@ function queryFireBase(){
     var numOfResults = 10;
     var tracker = 0;
     var artistName = '';
+    var imgURL = '';
     var hotness_score;
     $( "#hot_artists" ).empty();
     i = 0;
@@ -347,8 +348,9 @@ function queryFireBase(){
           artistName = array[i].name;
           hotness_score = array[i].hotttnesss;
           song = array[i].songs[0].title; 
+          if (array[i].images[0].url != undefined) { imgURL = array[i].images[0].url; }
           console.log("artist name is: " + artistName + " hotness score is: " + hotness_score + " top song: " + song);
-          $( "#hot_artists" ).append( "<li>" + artistName + ": " + "score: " +hotness_score + " top song: " +song +"</li>" );
+          $( "#hot_artists" ).append( "<li><div class = 'song'>"+song+"</div><div class = 'artist'>" + artistName + ": score: " +hotness_score +"</div></li>" );
           topTenArtists.push(array[i]);
           console.log("topTenArtists array in displayFireBaseResults is : " + topTenArtists);
         }
