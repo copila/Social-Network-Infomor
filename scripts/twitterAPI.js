4abedda5bb6080eff9299906ecd69dadf287c694
zaidhaque <zaidhaque@gmail.com>
2016-03-15 19:08:08 -0400

    codebird again?

--- scripts/twitterAPI.js
+++ scripts/twitterAPI.js
@@ -1,36 +1,45 @@
 // $('#twitterAuth').on('click', authTwitter);
 
 var cb = new Codebird;
-cb.setConsumerKey("Xy0iOHRXi8GFPajsVJj0waIQB", "Hd8I5EOyq5yI9diPLNLT7cfB3QZ2fAw2UY1mWqIBn9kJQHTDp6");
+cb.setConsumerKey("myMJ6eT51X95q7ksmPrHfS5tI", "Ldp8SM2SVQ6lx4AP6qGL8QAYYct3DXGGgubhIGkYbfp1Z6NJBB");
+
+var twitterToken = "";
+var twitterSecret = "";
 
 function authTwitter(){
-  cb.__call(
-      "oauth_requestToken",
-      {oauth_callback: "oob"},
-      function (reply) {
-          // stores it 
-          cb.setToken(reply.oauth_token, reply.oauth_token_secret);
-   
-          // gets the authorize screen URL 
-          cb.__call(
-              "oauth_authorize",
-              {},
-              function (auth_url) {
-                  window.codebird_auth = window.open(auth_url);
-              }
-          );
-      }
-  );
+    
+    cb.__call(
+        "oauth_requestToken",
+        {oauth_callback: "oob"},
+        function (reply) {
+            // stores it 
+            cb.setToken(reply.oauth_token, reply.oauth_token_secret);
+     
+            // gets the authorize screen URL 
+            cb.__call(
+                "oauth_authorize",
+                {},
+                function (auth_url) {
+                    window.codebird_auth = window.open(auth_url);
+                }
+            );
+        }
 
-  cb.__call(
-    "oauth_accessToken",
-    {oauth_verifier: document.getElementById("PINFIELD").value},
-    function (reply) {
-        // store the authenticated token, which may be different from the request token (!) 
-        cb.setToken(reply.oauth_token, reply.oauth_token_secret);
- 
-        // if you need to persist the login after page reload, 
-        // consider storing the token in a cookie or HTML5 local storage 
-    }
-  );
+    );
+
+}
+
+function getTwitterAccessToken(){
+    console.log(document.getElementById("PINFIELD").value);
+    cb.__call(
+        "oauth_accessToken",
+        {oauth_verifier: document.getElementById("PINFIELD").value},
+        function (reply) {
+            // store the authenticated token, which may be different from the request token (!) 
+            cb.setToken(reply.oauth_token, reply.oauth_token_secret);
+            console.log(reply)
+            // if you need to persist the login after page reload, 
+            // consider storing the token in a cookie or HTML5 local storage 
+        }
+    );
 }
