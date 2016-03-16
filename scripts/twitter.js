5a07faf2dbd81fcc0b7f68435dec245f3f899247
zaidhaque <zaidhaque@gmail.com>
2016-03-15 23:34:46 -0400

    twitter

--- scripts/twitter.js
+++ scripts/twitter.js
@@ -1,4 +1,7 @@
 var ref = new Firebase("https://social-informor.firebaseio.com");
+
+$('#get_twitter_handles').on('click', ref.authWithOAuthPopup);
+
 ref.authWithOAuthPopup("twitter", function(error, authData) {
   if (error) {
     console.log("Login Failed!", error);
