6b7e49969af909ac6bb418c722f9ad28f7ab1714
zaidhaque <zaidhaque@gmail.com>
2016-03-15 20:51:34 -0400

    twitter from firebase

--- scripts/twitter.js
+++ scripts/twitter.js
@@ -1,17 +1,8 @@
-
-
 var ref = new Firebase("https://social-informor.firebaseio.com");
-
-$('#sign-in-with-twitter').on('click', 'input', function() { twitOath() });
-
-function twitOath(){
-	ref.authWithOAuthPopup("twitter", function(error, authData) {
-		  if (error) {
-		    console.log("Login Failed!", error);
-		  } else {
-		    console.log("Authenticated successfully with payload:", authData);
-		  }
-	});
-}
-       
-
+ref.authWithOAuthPopup("twitter", function(error, authData) {
+  if (error) {
+    console.log("Login Failed!", error);
+  } else {
+    console.log("Authenticated successfully with payload:", authData);
+  }
+});
